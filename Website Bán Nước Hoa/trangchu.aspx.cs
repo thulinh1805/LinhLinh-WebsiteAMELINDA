2b68f7fe374c765674b1ab09dc03d376f5215020
suulii1805_beyes <72753861+thulinh1805@users.noreply.github.com>
2022-05-20 00:29:04 +0700

    Website AMENLINDA

---
+++ Website Bán Nước Hoa/trangchu.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website_Bán_Nước_Hoa
+{
+    public partial class trangchu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["dangnhap"].ToString() == "1")
+            {
+                //doimk.Visible = true;
+                dangnhapdangky.InnerHtml =
+                "<span style='color:white;'> Xin chào  " + Session["hoten"].ToString() + "</span>"
+              + "<span style='margin-left:5px;'>|</span> <a href='dangxuat.aspx' style='font-size: 16px;'><span class='icon-dx'><i class='fas fa-sign -out-alt'></i></span><span class='dangxuat' style='font-size: 16px;'>Đăng xuất</span></a> ";
+                DateTime now = DateTime.Now;
+                //   Response.Write("<script>console.log(now.toString())</script>");
+                /*System.Diagnostics.Debug.WriteLine(now);*/
+            }
+            else
+            {
+                //doimk.Visible = true;
+                /* dangnhapdangky.InnerHtml =
+                     "<a href=\"dangnhap.aspx\"><span class=\"icon-dkdn\" ><i class=\"fa fa-user\"></i></span></a>"+"<a href=\"dangnhap.aspx\">Đăng nhập</a><span>|</span>" +
+                            "<a href=\"dangky.aspx\">Đăng ký</a> ";
+                 Response.Write("<a href='./SignIn.aspx'><span><i class=\"fa fa-user\"></i></span> <span> Đăng Ký/ Đăng Nhập </span></a>");*/
+                dangnhapdangky.InnerHtml = "<span class='icon-dkdn'><i class='fas fa-user'></i></span> <span class='okk'><a href='dangnhap.aspx' style='font-size: 16px;'>Đăng nhập</a> | <a href='dangky.aspx' style='font-size: 16px;'>Đăng ký</a></span>";
+            }
+
+            List<Product> listSP = (List<Product>)Session["giohang"];
+            int dem = 0;
+            foreach (Product p in listSP)
+            {
+                dem++;
+            }
+            cartnumber.InnerHtml = "" + dem;
+        }
+    }
+}
